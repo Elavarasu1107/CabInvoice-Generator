0fdc5defea032851f9d24ce2de8a97c9ec9af853
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-06-16 19:43:18 +0530

    [Elavarasu] UC2-Multiple Rides

--- CabInvoice_Testing/UnitTest1.cs
+++ CabInvoice_Testing/UnitTest1.cs
@@ -1,3 +1,4 @@
+using CabInvoice;
 namespace CabInvoice_Testing
 {
     public class Tests
@@ -6,9 +7,18 @@ namespace CabInvoice_Testing
         public void GivenDistanceAndTime_ShouldReturnExpectedTotalFare()
         {
             double distance = 10;
-            int time = 5, expected = 105;
-            CabInvoice.InvoiceGenerator getMethod = new CabInvoice.InvoiceGenerator(distance, time);
-            Assert.AreEqual(expected, getMethod.CalculateFare());
+            int time = 10, expected = 110;
+            InvoiceGenerator getMethod = new InvoiceGenerator();
+            Ride ride = new Ride(distance, time);
+            Assert.AreEqual(expected, getMethod.CalculateFare(ride));
+        }
+        [Test]
+        public void GivenMultipleRideData_ShouldReturnExpectedTotalFare()
+        {
+            int expected = 215;
+            InvoiceGenerator getMethod = new InvoiceGenerator();
+            Ride[] ride = { new Ride(10, 10), new Ride(10, 5) };
+            Assert.AreEqual(expected, getMethod.MultipleRides(ride));
         }
     }
 }
